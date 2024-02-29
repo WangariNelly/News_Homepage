c11b5dacefd1acfc21e19bce796e036e0d5f2255
WangariNelly <nellywangari30@gmail.com>
2024-02-29 12:16:26 +0300

    Updated changes

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-const 
+function openNav() {
+    document.getElementById("openbutton").style.right  = "0";
+}
+
+function closeNav(){
+    document.getElementById("closebutton").style.right = "-200px";
+}
